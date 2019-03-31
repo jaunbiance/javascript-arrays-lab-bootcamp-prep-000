ffce221206cd6a1aba5a8b4bf9b5cf3af8ce2c3a
Jessica Lin <jessicaa.lin@gmail.com>
2019-03-31 23:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ kittens.push('Milo', 'Otis', 'Garfield');
 function destructivelyAppendKitten(name){
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
