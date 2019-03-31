66dd5db775d28ef1888858c874f0eb8c4ff9b5f2
Jessica Lin <jessicaa.lin@gmail.com>
2019-03-31 23:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var kittens = []; //define your array here
-kittens.push()
+kittens.push('Milo', 'Otis', 'Garfield');
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
